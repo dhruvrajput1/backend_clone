69928f90285f2c06e2583a0af2026bad27e26876
Dhruv Rajput <dhruvrajput2001@gmail.com>
2024-05-26 13:26:58 +0530

    updates app.js and created utils for apiError, apiResponse and async handler

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import dotenv from "dotenv";
-import express from "express";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path: "./env"
 });
 
-connectDB();
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is live and running on port ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("error in connectDB in main index file: " + err.message);
+})
 
-const app = express();
