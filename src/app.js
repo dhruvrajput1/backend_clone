e9d81ab7efd867f7891a918660f6e567e8c990a1
Dhruv Rajput <dhruvrajput2001@gmail.com>
2024-05-27 13:52:00 +0530

    created user model and video model with refresh and access tokens

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 
 const app = express();
-
+                      
 app.use(cors({  // use is used in handling middlewares and configuratons
     origin: process.env.CORS_ORIGIN,
 })); 
@@ -12,9 +12,6 @@ app.use(express.json({limit: "20kb"})); // when data comes in form of JSON
 app.use(express.urlencoded({urlencoded: true, limit: "20kb"})); // when data comes in form of link string
 app.use(express.static("public")); // used to store images and files
 app.use(cookieParser()); // it is used to read and update the cookies stored in user browser
-
-
-
 
 
 export {app};
