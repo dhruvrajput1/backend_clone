91f460942be0a401abf36d84df8e88943b9f8ef1
Dhruv Rajput <dhruvrajput2001@gmail.com>
2024-06-29 17:51:12 +0530

    fixed sending user while creating comment

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -3,6 +3,7 @@ import { Comment } from "../models/comment.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { User } from "../models/user.model.js";
 
 
 const getVideoComments = asyncHandler(async (req, res) => {
@@ -72,6 +73,8 @@ const addComment = asyncHandler(async (req, res) => {
         const { text } = req.body;
         const userId = await req.user._id;
 
+        const user = await User.findById(userId);
+
         if(text === "") {
             throw new ApiError(400, "Comment cannot be empty");
         }
@@ -79,7 +82,7 @@ const addComment = asyncHandler(async (req, res) => {
         const comment = await Comment.create({
             content: text,
             video: videoId,
-            owner: userId
+            owner: user
         });
     
         if(!comment) {
